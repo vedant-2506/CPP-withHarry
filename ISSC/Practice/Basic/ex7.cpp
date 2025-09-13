ad11e809343970311bdf5ba140996430f8cbd853
vedant-2506 <swamidivate2506@gmail.com>
2025-09-13 17:00:54 +0530

    Swap two numbers using a third variable.

--- ISSC/Practice/Basic/ex7.cpp
+++ ISSC/Practice/Basic/ex7.cpp
@@ -1,1 +1,20 @@
-777
+//Swap two numbers using a third variable.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=0,b=0,c=0;
+    
+
+    cout<<"Enter  first number  :";
+    cin>>a;
+    cout<<"Enter  second number  :";
+    cin>>b;
+
+    c=a;
+    a=b;
+    b=c;
+
+    cout<<"Number is "<<a << " and "<< b<<"\n";
+    return 0;
+}
