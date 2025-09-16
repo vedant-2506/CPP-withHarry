84c9a461826b8d75ecf113bc4182c127b7073261
vedant-2506 <swamidivate2506@gmail.com>
2025-09-16 22:39:03 +0530

    Get and display the size of int, float, double, and char using sizeof.

---
+++ ISSC/Practice/Basic/ex8.cpp
@@ -0,0 +1,14 @@
+//Get and display the size of int, float, double, and char using sizeof.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    cout<<"Size of int "<<sizeof(int)<<" bytes \n";
+    cout<<"Size of float "<<sizeof(float)<<" bytes \n";
+    cout<<"Size of double "<<sizeof(double)<<" bytes \n";
+    cout<<"Size of char "<<sizeof(char)<<" bytes \n";
+
+    return 0;
+}
