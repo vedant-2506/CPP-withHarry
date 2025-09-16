c9d73192d60106cf20e1a5d3a49121a20d421352
vedant-2506 <swamidivate2506@gmail.com>
2025-09-16 22:58:13 +0530

    Assign a character constant to a variable and print it

---
+++ ISSC/Practice/Basic/ex9.cpp
@@ -0,0 +1,11 @@
+//Assign a character constant to a variable and print it.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch ='V';
+    cout<<"character is "<<ch<<" .\n";
+    
+    return 0; 
+}
