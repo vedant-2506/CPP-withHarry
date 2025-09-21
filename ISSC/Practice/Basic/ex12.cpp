03fcfbaf511bc5275da0d17fb01eccac5115cd0a
vedant-2506 <swamidivate2506@gmail.com>
2025-09-21 17:06:48 +0530

    Accept an integer 𝑥 and compute

---
+++ ISSC/Practice/Basic/ex12.cpp
@@ -0,0 +1,17 @@
+//​Accept an integer 𝑥 and compute
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x =0,a=0,b=0;
+    cout<<"Enter a x value  :";
+    cin>>x;
+
+    a =x*x ;
+    b= a +2*x ;
+    cout<<"answer of x^2 is  "<<a<<".\n";
+    cout<<"answer of (x^2 +2x) is  "<<b<<".\n";
+    
+    return 0; 
+}
