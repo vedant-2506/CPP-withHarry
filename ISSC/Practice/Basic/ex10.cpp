ab4f5301930edbfe4f3ba104a08052007654a9af
vedant-2506 <swamidivate2506@gmail.com>
2025-10-03 20:50:46 +0530

    ​Read a character from the user, assign to a variable, and print it.

---
+++ ISSC/Practice/Basic/ex10.cpp
@@ -0,0 +1,14 @@
+//​Read a character from the user, assign to a variable, and print it.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch =0;
+    cout<<"Enter a one charator :";
+    cin>>ch;
+
+    cout<<"Character is "<<ch<<".\n";
+    
+    return 0; 
+}
