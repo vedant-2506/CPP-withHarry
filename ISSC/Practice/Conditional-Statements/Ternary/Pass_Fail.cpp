d872aa0cede2918c76aeb0cd1f94d6a16ec67dfe
vedant-2506 <swamidivate2506@gmail.com>
2025-10-05 23:48:53 +0530

    Assign remarks (Pass/Fail) using conditional operators based on marks.

---
+++ ISSC/Practice/Conditional-Statements/Ternary/Pass_Fail.cpp
@@ -0,0 +1,16 @@
+//​Assign remarks ("Pass"/"Fail") using conditional operators based on marks.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks;
+    cout << "Enter your marks: ";
+    cin >> marks;
+
+    // Assuming pass mark = 40
+    string result = (marks >= 40) ? "Pass" : "Fail";
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
