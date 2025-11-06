614107eebffcc6a6b596c02e030a605d1b8b90d6
vedant-2506 <swamidivate2506@gmail.com>
2025-11-06 20:32:43 +0530

    Reverse number

---
+++ ISSC/Practice/LOOP/While/Reverse.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, rev = 0, rem;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    while (num != 0) {
+        rem = num % 10;
+        rev = rev * 10 + rem;
+        num /= 10;
+    }
+
+    cout << "Reversed Number = " << rev;
+    return 0;
+}
