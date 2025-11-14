f03e7ccba78f35fb65b1f4e90ce24cd856a91d60
vedant-2506 <swamidivate2506@gmail.com>
2025-11-14 07:41:43 +0530

    Print a star pattern based on user input (height)

---
+++ ISSC/Practice/LOOP/While/StarPattern.cpp
@@ -0,0 +1,30 @@
+//Print a square pattern based on user input (height)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter height of pyramid: ";
+    cin >> n;
+
+    int i = 1;
+    while (i <= n) {
+
+        int spaces = 1;
+        while (spaces <= n - i) {
+            cout << " ";
+            spaces++;
+        }
+
+        int stars = 1;
+        while (stars <= 2*i - 1) {
+            cout << "*";
+            stars++;
+        }
+
+        cout << endl;
+        i++;
+    }
+    return 0;
+}
