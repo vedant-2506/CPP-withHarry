e66ed19c3081abc3c725567cdc3332ac7bd0f0e1
vedant-2506 <swamidivate2506@gmail.com>
2025-11-21 09:23:04 +0530

    Print various formatted patterns pyramid using nested loops.

---
+++ ISSC/Practice/LOOP/NestedLoop/Pyramid.cpp
@@ -0,0 +1,27 @@
+//Print various formatted patterns pyramid using nested loops.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // Print stars
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
