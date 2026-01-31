b7a3b924ec0f519ec872c56910db5ec596edf134
Vedant Divate <swamidivate2506@gmail.com>
2026-01-31 18:00:03 +0530

    Read an integer from the user and print it
    This program prompts the user to enter an integer and displays the entered value.

---
+++ ISSC/Practice/Basic/PrintInt.cpp
@@ -0,0 +1,17 @@
+// Read an integer from the user and print it.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n=0;
+
+    cout<<"Enter integer value from user :";
+
+    cin>>n;
+
+    cout<<"Integr value is "<<n<<"\n";
+
+    return 0;
+}
