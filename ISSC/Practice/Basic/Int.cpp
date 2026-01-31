defa8b863bc8713a9e6f7a64d75a222304c852ed
Vedant Divate <swamidivate2506@gmail.com>
2026-01-31 17:58:39 +0530

    Print an integer constant.

---
+++ ISSC/Practice/Basic/Int.cpp
@@ -0,0 +1,16 @@
+// Print an integer constant.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    float b=7.5;
+
+    cout<<"Integer is "<<a<<"\n";
+
+    cout<<"floating point is "<<b<<"\n";
+
+    return 0;
+
+}
