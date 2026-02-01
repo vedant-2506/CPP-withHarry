e97b66c327b5d8f70de173a32688e9bf719cc80d
Vedant Divate <swamidivate2506@gmail.com>
2026-02-01 18:48:51 +0530

    Read two integers from the user and display both.

---
+++ ISSC/Practice/Basic/TwoINT.cpp
@@ -0,0 +1,18 @@
+//Read two integers from the user and display both.
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=0 ,b=0;
+
+    cout<<"Enter integer value from user :";
+
+    cin>>a>>b;
+
+    cout<<"Integr value is "<<a <<"and"<< b<<"\n";
+
+    return 0;
+}
